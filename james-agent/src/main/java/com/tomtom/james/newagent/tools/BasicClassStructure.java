fc98deba6d7be8daca820d315d2e265c57dca36f
Piotr Wielgolaski <piotr.wielgolaski@tomtom.com>
2020-01-15 12:33:10 +0100

    fix: fix ConcurrentModificationException
    it was reported in com.tomtom.james.newagent.JamesHQ.prapareObjectiveForSignleInformationPoint(JamesHQ.java:107)

--- james-agent/src/main/java/com/tomtom/james/newagent/tools/BasicClassStructure.java
+++ james-agent/src/main/java/com/tomtom/james/newagent/tools/BasicClassStructure.java
@@ -14,16 +14,13 @@ public class BasicClassStructure implements ClassStructure {
 
     @Override
     public Set<Class> getChildren(String className) {
-        return (container.get(className) != null) ? container.get(className) : new HashSet<>();
+        return (container.get(className) != null) ? new HashSet<>(container.get(className) ): new HashSet<>();
     }
 
-    // FIXME - if you want to implement multithreaded classScanner there is the point that multithreading pitfall is waiting for you - it's not threadsafe !!!!!
     @Override
-    public void addChild(String className, Class<?> clazz) {
-        container.computeIfAbsent(className, key -> new HashSet<>());
-        Set<Class> classSet = container.get(className);
-        classSet.add(clazz);
-        container.put(className, classSet);
+    public synchronized  void addChild(String className, Class<?> clazz) {
+        container.computeIfAbsent(className, key -> new HashSet<>())
+                .add(clazz);
     }
 
     @Override
