8945924bdccc3db1b942d523ca77031f31d5282d
Piotr Wielgolaski <piotr.wielgolaski@tomtom.com>
2022-01-10 14:42:51 +0100

    fix: handle properly removal of information point
    it was failing, leading to connection leaks

--- james-agent-io/src/main/java/com/tomtom/james/store/informationpoints/io/InformationPointDTO.java
+++ james-agent-io/src/main/java/com/tomtom/james/store/informationpoints/io/InformationPointDTO.java
@@ -3,6 +3,7 @@ package com.tomtom.james.store.informationpoints.io;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tomtom.james.common.api.informationpoint.InformationPoint;
 import com.tomtom.james.common.api.informationpoint.Metadata;
+import java.util.Objects;
 
 public abstract class InformationPointDTO {
 
@@ -34,5 +35,26 @@ public abstract class InformationPointDTO {
     @JsonIgnore
     public String getMethodReference(){
         return className+"!"+methodName;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final InformationPointDTO that = (InformationPointDTO)o;
+        return Objects.equals(className, that.className) && Objects.equals(methodName, that.methodName)
+               && Objects.equals(sampleRate, that.sampleRate) && Objects.equals(successSampleRate,
+                                                                                that.successSampleRate)
+               && Objects.equals(errorSampleRate, that.errorSampleRate) && Objects.equals(
+            successExecutionThreshold, that.successExecutionThreshold) && Objects.equals(metadata, that.metadata)
+               && Objects.equals(baseScriptPath, that.baseScriptPath) && Objects.equals(scriptPath,
+                                                                                        that.scriptPath)
+               && Objects.equals(version, that.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(className, methodName, sampleRate, successSampleRate, errorSampleRate, successExecutionThreshold,
+                            metadata, baseScriptPath, scriptPath, version);
     }
 }
